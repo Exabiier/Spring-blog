1b7578315e8cd04efccdb3127f5379371dc2af1a
Exabiier <exabiier.bars@gmail.com>
2022-11-15 13:40:44 -0600

    first commit

---
+++ src/main/java/codeup/codeupspringblog/controllers/HelloController.java
@@ -0,0 +1,19 @@
+package codeup.codeupspringblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String Hello(){
+        return "Hello World";
+    }
+
+
+
+}
